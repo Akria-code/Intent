8a6793e5b9b64431db376a7662310546173d83ef
wm <190204291@qq.com>
2020-06-20 14:25:23 +0800

    button3 to phone

--- app/src/main/java/com/example/intent/ThirdActivity.java
+++ app/src/main/java/com/example/intent/ThirdActivity.java
@@ -2,7 +2,12 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 /*
 * 为了让你能够更加直观地理解，我们来自己建立一个活动，让它也能响应打开网页的Intent。
 * */
@@ -12,5 +17,14 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        Button button=findViewById(R.id.button_3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }
+        });
     }
 }
